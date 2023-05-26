175a88e368aa45d05cfe5f9e6ff6534e6a4e8c50
Isaac Nino <114028749+IsaacNino@users.noreply.github.com>
2023-05-26 12:20:10 -0500

    imported new components
    changed /main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,16 +1,32 @@
-import { useEffect, useState } from 'react'; // Import statement for useState
-import { MovieCard } from '../movie-card/movie-card'; // Import statement for MovieCard
-import { MovieView } from '../movie-view/movie-view'; // Import statement for MovieView
+import { useEffect, useState } from 'react'; //Import statement for useState
+import { MovieCard } from '../movie-card/movie-card'; //Import statement for MovieCard
+import { MovieView } from '../movie-view/movie-view'; //Import statement for MovieView
+import { LoginView } from '../login-view/login-view';//Import statement for LoginView
+import { SignupView } from '../signup-view/signup-view'; //Import statement for SignupView
 
 export const MainView = () => { // MainView component
+    const storedUser = JSON.parse(localStorage.getItem('user')); //stores the user in localStorage
+    const storedToken = localStorage.getItem('token'); //stores the token in localStorage
     const [movies, setMovies] = useState([]); //useState hook to store the state of the list of movies
-
     const [selectedMovie, setSelectedMovie] = useState(null); //useState hook to store the state of the selected movie
+    const [user, setUser] = useState(storedUser? storedUser : null); //useState hook to store the state of the user
+    const [token, setToken] = useState(storedToken? storedToken : null); //useState hook to store the state of the token
 
     useEffect(() => { //useEffect hook to retrieve the list of movies when the component is mounted
-        fetch('https://themovieapi.herokuapp.com/movies') //fetches the list of movies from the API
+        if (!token) { //if the token is false, returns
+            return;
+
+            fetch('https://themovieapi.herokuapp.com/movies', { //fetches the list of movies from the API
+                headers: { Authorization: 'Bearer ${token}' } //passes the token to the API call
+            })
+        }
+        
+        fetch('https://themovieapi.herokuapp.com/movies', { //fetches the list of movies from the API
+            headers: { Authorization: 'Bearer ${token}' }//passes the token to the API call
+        })
             .then((response) => response.json()) //converts the response to JSON format
-            .then((data) => { //stores the data in a state
+            .then((movies) => { 
+                console.log(data); //logs the data
                 
                 //maps over the list
                 const moviesFromApi = data.map((movie) => { //maps over the list
@@ -31,13 +47,27 @@ export const MainView = () => { // MainView component
                     ImagePath: movie.ImagePath, //movie.ImagePath prop
                     Featured: movie.Featured //movie.Featured prop
                 };
-                })
-                setMovies(moviesFromApi); //sets the state of the list of movies
+                });
+                
+                setMovies(movies); //setMovies prop
             })
             .catch((error) => { //logs any errors
                 console.log('Error retrieving movies:', error); //logs any errors
             });
-    }, []);
+    }, [token]);
+
+    if (!user) {
+        return (
+            <>
+                <LoginView onLoggedIn={(user, token) => { //If the user is false, returns the LoginView component
+                    setUser(user); //setUser prop
+                    setToken(token); //setToken prop
+                }} />
+                or
+                <SignUpView />
+            </>
+        );
+    }
 
     if (selectedMovie) {
         return <MovieView movie={selectedMovie} onBackClick={() => //If the selected movie is true, returns the MovieView component
@@ -49,8 +79,17 @@ export const MainView = () => { // MainView component
     if (movies.length === 0) { //If the list of movies is empty, returns this message
         return <div className='main-view'>The list is empty!</div>; //If the list of movies is empty, returns this message
     }
+    
     return (//Returns the list of movies
         <div>
+            <button onClick={() => { //onClick event handler
+                setUser(null); //setUser prop
+                setToken(null); //setToken prop
+                localStorage.clear(); //clears localStorage
+            }}>
+                Logout
+            </button>
+
             {movies.map((movie) => { //Maps over the list of movies
                 return <MovieCard 
                 key={movie.id} //Key prop

ae98e1d3c75e4767e8a8323170d1b635381df0d4
Isaac Nino <114028749+IsaacNino@users.noreply.github.com>
2023-10-03 19:58:57 -0600

    imported container, root, and main-view
    changed /index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,18 @@
-import { createRoot } from 'react-dom/client';
-import { MainView } from './components/main-view/main-view';
+import { createRoot } from 'react-dom/client'; //import statement for createRoot function
+import { MainView } from './components/main-view/main-view'; //import statement for main-view.jsx file
+import Container from "react-bootstrap/Container"; //import statement for Container from react-bootstrap
 
-import "./index.scss";
+import "./index.scss"; //import statement for index.scss file
 
-const MyFlixApplication = () => {
-    return <MainView />;
+const MyFlixApplication = () => { 
+    return (
+        <Container>
+            <MainView />;
+        </Container>
+    );
 };
 
-const container = document.querySelector('#root');
-const root = createRoot(container);
+const container = document.querySelector('#root'); 
+const root = createRoot(container); 
 
 root.render(<MyFlixApplication />);
